0aa9348fc9890d49fd5a855c5fca3301bb2f65bf
LuatNg <nvluathcmut@gmail.com>
2025-02-14 14:25:35 +0700

    ✅ Fix: Customize the Default User Admin

--- myproject/myapp/admin.py
+++ myproject/myapp/admin.py
@@ -1,4 +1,18 @@
-from django.contrib import admin
+
 from .models import Task
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import User
+
+# Unregister default User model
+admin.site.unregister(User)
+
+# Customize User admin
+class CustomUserAdmin(UserAdmin):
+    list_display = ('username', 'email', 'first_name', 'last_name', 'is_staff', 'date_joined')
+    search_fields = ('username', 'email')
+
+# Re-register with customization
+admin.site.register(User, CustomUserAdmin)
 
 admin.site.register(Task)
