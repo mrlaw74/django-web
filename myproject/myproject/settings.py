6e6ddde2e41af48ce8eba91e32a3507381e42801
LuatNg <nvluathcmut@gmail.com>
2025-02-15 14:37:54 +0700

    Enable JWT Authentication in Django REST Framework

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -132,8 +132,9 @@ LOGIN_URL = '/login/'
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.SessionAuthentication',
-        'rest_framework.authentication.TokenAuthentication',
+        # 'rest_framework.authentication.SessionAuthentication',
+        # 'rest_framework.authentication.TokenAuthentication',
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
     ],
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
